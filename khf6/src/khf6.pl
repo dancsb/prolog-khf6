9d2ce3bb64c27f3fb52e99c07df9cc2dde2e137d
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-11-04 15:43:09 +0100

    really done

--- khf6/src/khf6.pl
+++ khf6/src/khf6.pl
@@ -3,15 +3,15 @@
 osszeg_szukites_core([], _, [], [], []).
 
 osszeg_szukites_core([Fx-Fy | RestFs], sor(I, Db), [Dir | RestILs], [NewB | B], [NewE | E]) :-
-    (((Fx = I, Dir = [e,w]); (Fx is I - 1, Dir = [s]); (Fx is I + 1, Dir = [n])) -> NewB = Fx-Fy, NewE = [];
-        (((Fx = I, subseq0(Dir, [e,w])); (Fx is I - 1, member(s, Dir)); (Fx is I + 1, member(n, Dir))) -> NewE = Fx-Fy; NewE = []),
+    (((Fx = I, subseq0([e,w], Dir)); (Fx is I - 1, Dir = [s]); (Fx is I + 1, Dir = [n])) -> NewB = Fx-Fy, NewE = [];
+        (((Fx = I, (member(e, Dir); member(w, Dir))); (Fx is I - 1, member(s, Dir)); (Fx is I + 1, member(n, Dir))) -> NewE = Fx-Fy; NewE = []),
         NewB = []
     ),
     osszeg_szukites_core(RestFs, sor(I, Db), RestILs, B, E).
 
 osszeg_szukites_core([Fx-Fy | RestFs], oszl(J, Db), [Dir | RestILs], [NewB | B], [NewE | E]) :-
-    (((Fy = J, Dir = [n,s]); (Fy is J - 1, Dir = [e]); (Fy is J + 1, Dir = [w])) -> NewB = Fx-Fy, NewE = [];
-        (((Fy = J, subseq0(Dir, [n,s])); (Fy is J - 1, member(e, Dir)); (Fy is J + 1, member(w, Dir))) -> NewE = Fx-Fy; NewE = []),
+    (((Fy = J, subseq0([n,s], Dir)); (Fy is J - 1, Dir = [e]); (Fy is J + 1, Dir = [w])) -> NewB = Fx-Fy, NewE = [];
+        (((Fy = J, (member(n, Dir); member(s, Dir))); (Fy is J - 1, member(e, Dir)); (Fy is J + 1, member(w, Dir))) -> NewE = Fx-Fy; NewE = []),
         NewB = []
     ),
     osszeg_szukites_core(RestFs, oszl(J, Db), RestILs, B, E).
