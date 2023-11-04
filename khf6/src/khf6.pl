3f7ec9dd998f9515b91c1c6279d37d7cf9bae66b
Dancs Balázs <100741583+dancsb@users.noreply.github.com>
2023-11-04 11:36:11 +0100

    it no worke

--- khf6/src/khf6.pl
+++ khf6/src/khf6.pl
@@ -1,1 +1,26 @@
-%pls help me
+:- use_module(library(lists)).
+
+osszeg_szukites_core([], _, [], [], []).
+
+osszeg_szukites_core([Fx-Fy | RestFs], sor(I, Db), [Dir | RestILs], [NewB | B], [NewE | E]) :-
+    ((Fx = I, Dir = [e,w]); (Fx is I - 1, Dir = [s]); (Fx is I + 1, Dir = [n]) -> NewB = Fx-Fy;
+        ((Fx = I, subseq0(Dir, [e,w])); (Fx is I - 1, member(s, Dir)); (Fx is I + 1, member(n, Dir)) -> NewE = Fx-Fy; NewE = asd),
+        NewB = asd
+    ),
+    osszeg_szukites_core(RestFs, sor(I, Db), RestILs, B, E).
+
+osszeg_szukites_core([Fx-Fy | RestFs], oszl(J, Db), [Dir | RestILs], [NewB | B], [NewE | E]) :-
+    ((Fy = J, Dir = [n,s]); (Fy is J - 1, Dir = [e]); (Fy is J + 1, Dir = [w]) -> NewB = Fx-Fy;
+        ((Fy = J, subseq0(Dir, [n,s])); (Fy is J - 1, member(e, Dir)); (Fy is J + 1, member(w, Dir)) -> NewE = Fx-Fy; NewE = asd),
+        NewB = asd
+    ),
+    nl,write(NewB),nl,write(NewE),
+    osszeg_szukites_core(RestFs, oszl(J, Db), RestILs, B, E).
+
+osszeg_szukites(Fs, Osszegfeltetel, ILs0, ILs) :-
+    osszeg_szukites_core(Fs, Osszegfeltetel, ILs0, B, E),
+    length(B, Bl),length(E, El),
+    nl,nl,write(Bl),nl,write(El),nl,
+    nl,write(B),nl,write(E),nl,
+    ILs=[thisistheend],
+    !.
